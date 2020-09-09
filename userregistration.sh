8ae924a592ff4f978c13c9a019acaa22c51d0d53
unknown <manishasingh2203@gmail.com>
2020-09-09 21:09:22 +0530

    UC3 done

--- userregistration.sh
+++ userregistration.sh
@@ -4,9 +4,11 @@ echo "     USER REGISTRATION SYSTEM     "
 #patterns
 Firstname="^[A-Z]+[a-z]*$"
 Lastname="^[A-Z]+[a-z]*$"
+email="^[a-z]+[0-9]*([._-+][0-9a-z]+)*@[0-9a-z]+[.][a-z]{2,4}([.][a-z]{2})*$"
 #user input
 read -p "Enter First Name: " enter_Firstname
 read -p "Enter First Name: " enter_Lastname
+read -p "Enter EMAIL ID: " enter_email
 #Validation Check
 if [[ $enter_Firstname =~ $Firstname ]]
 then
@@ -20,4 +22,9 @@ then
 else
         echo Invalid Last Name
 fi
-
+if [[ $enter_email =~ $email ]]
+then
+        echo "Email Id: $enter_email"
+else
+        echo Invalid Email Id
+fi
